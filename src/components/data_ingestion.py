f23dc8186c6ffdeeea02248e8e6877dd94a74798
Shubham Raj <shubmrj@gmail.com>
2026-02-17 12:26:39 +0530

    model trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,10 @@ from sklearn.model_selection import train_test_split
 
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
+
 # dataclass decorator help me write code without constructor
 @dataclass
 class DataIngestionConfig:
@@ -52,5 +56,10 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
-        
+    train_data,test_data = obj.initiate_data_ingestion()
+    
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+     
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))   
